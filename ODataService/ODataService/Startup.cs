d8ea3f59da5107ad97fd7c9e47b838d007525b9f
Oleh Kikhtov <oleh.kikhtov@sailpoint.com>
2021-06-22 11:18:52 +0300

    Add controller

--- ODataService/ODataService/Startup.cs
+++ ODataService/ODataService/Startup.cs
@@ -1,15 +1,11 @@
+using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Extensions;
+using ODataService.Models;
 
 namespace ODataService
 {
@@ -25,26 +21,22 @@ namespace ODataService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(mvcOptions =>
+                mvcOptions.EnableEndpointRouting = false);
+            services.AddMvc();
+            services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+            builder.EntitySet<Product>("Products");
+            app.UseMvc(routeBuilder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+                routeBuilder.EnableDependencyInjection();
+                routeBuilder.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
+                routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
             });
         }
     }
